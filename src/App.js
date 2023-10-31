0c674c460dd0cc3b9d0954331776db08f3d71413
Devin Lewis <devinlewis@pursuit.org>
2023-10-31 09:47:37 -0400

    Setup Delay Frontend

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader/Loader";
 // COMPONENTS
 //const Example = React.lazy(() => import("./components/Example/Example"));
+const Delay = React.lazy(() => import("./components/Delay/Delay"));
 function App() {
     return (
         <React.Suspense fallback={<Loader />}>
             <Router>
                 <Routes>
                     <Route path="/" element={<h1>Hello world!</h1>} />
+                    <Route path="/delay" element={<Delay />} />
                 </Routes>
             </Router>
         </React.Suspense>
---
+++ src/components/Delay/Delay.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./Delay.css";
+function Delay() {
+    const initialFormData = {
+        dTime: 75,
+        dAmount: 50,
+        dFeedback: 100,
+    };
+    const [formData, setFormData] = useState(initialFormData);
+    return (
+        <div className="delay">
+            <h2 className="header">Delay</h2>
+            <div className="slider1">
+                <input
+                    type="range"
+                    min="1"
+                    max="100"
+                    value={formData.dTime}
+                    name="dTime"
+                    className="slider"
+                    onChange={(e) =>
+                        setFormData((prevFormData) => ({
+                            ...prevFormData,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }
+                />
+            </div>
+            <span className="time">Time</span>
+            <div className="slider2">
+                <input
+                    type="range"
+                    min="1"
+                    max="100"
+                    value={formData.dFeedback}
+                    className="slider"
+                    name="dFeedback"
+                    onChange={(e) =>
+                        setFormData((prevFormData) => ({
+                            ...prevFormData,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }
+                />
+            </div>
+            <span className="feedback">Feedback</span>
+            <div className="slider3">
+                <input
+                    type="range"
+                    min="1"
+                    max="100"
+                    value={formData.dAmount}
+                    className="slider"
+                    name="dAmount"
+                    onChange={(e) =>
+                        setFormData((prevFormData) => ({
+                            ...prevFormData,
+                            [e.target.name]: e.target.value,
+                        }))
+                    }
+                />
+            </div>
+            <span className="amount">Amount</span>
+            <div className="submit">
+                <input type="submit" value="Apply" />
+            </div>
+        </div>
+    );
+}
+
+export default Delay;
