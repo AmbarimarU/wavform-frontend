2d3d3a4f5dd7a2cca4615464093aa82bbd8d742c
Devin Lewis <devinlewis@pursuit.org>
2023-11-03 09:06:26 -0400

    Delay component fully functional

--- src/components/Delay/Delay.js
+++ src/components/Delay/Delay.js
@@ -1,20 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Delay.css";
+import * as Tone from "tone";
 function Delay() {
     const initialFormData = {
-        dTime: 75,
-        dAmount: 50,
-        dFeedback: 100,
+        dTime: 0,
+        dAmount: 0,
+        dFeedback: 0,
     };
     const [formData, setFormData] = useState(initialFormData);
+    //const [isPlaying, setIsPlaying] = useState(false);
+    async function loadAudio() {
+        try {
+            await player.load("https://tonejs.github.io/audio/loop/FWDL.mp3");
+        } catch (error) {
+            console.error("Error loading audio file:", error);
+        }
+    }
+
+    const player = new Tone.Player();
+    player.volume.value = formData.dAmount;
+    const feedbackDelay = new Tone.FeedbackDelay({
+        delayTime: formData.dTime * 0.01,
+        feedback: formData.dFeedback * 0.01,
+    });
+    player.connect(feedbackDelay);
+    feedbackDelay.toDestination();
+    const applyDelay = (e) => {
+        e.preventDefault();
+        if (Tone.context.state === "suspended") {
+            Tone.context.resume().then(() => {
+                player.start();
+            });
+        } else {
+            player.start();
+        }
+    };
+
+    useEffect(() => {
+        loadAudio();
+    }, [player]);
+
+    useEffect(() => {
+        feedbackDelay.delayTime.value = formData.dTime * 0.01;
+        feedbackDelay.feedback.value = formData.dFeedback * 0.01;
+    }, [formData, feedbackDelay.delayTime, feedbackDelay.feedback]);
     return (
         <div className="delay">
             <h2 className="header">Delay</h2>
             <div className="slider1">
                 <input
                     type="range"
-                    min="1"
-                    max="100"
+                    min="0"
+                    max="60"
                     value={formData.dTime}
                     name="dTime"
                     className="slider"
@@ -30,8 +67,8 @@ function Delay() {
             <div className="slider2">
                 <input
                     type="range"
-                    min="1"
-                    max="100"
+                    min="0"
+                    max="60"
                     value={formData.dFeedback}
                     className="slider"
                     name="dFeedback"
@@ -47,8 +84,9 @@ function Delay() {
             <div className="slider3">
                 <input
                     type="range"
-                    min="1"
-                    max="100"
+                    min="-60"
+                    max="6"
+                    step="1"
                     value={formData.dAmount}
                     className="slider"
                     name="dAmount"
@@ -66,9 +104,17 @@ function Delay() {
                     type="submit"
                     value="Reset"
                     className="button"
-                    onClick={(e) => setFormData(initialFormData)}
+                    onClick={(e) => {
+                        setFormData(initialFormData);
+                        player.stop();
+                    }}
                 />{" "}
-                <input type="submit" className="button" value="Apply" />
+                <input
+                    type="submit"
+                    className="button"
+                    value="Apply"
+                    onClick={(e) => applyDelay(e)}
+                />
             </div>
         </div>
     );
