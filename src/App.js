2e70b87b0c1f455de0ea447d7396e2537a97ba7c
Angel <angel@Angels-MacBook-Air.local>
2023-11-03 11:11:33 -0400

    created NotesSampler.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // COMPONENTS
 import Example from './components/Example/Example';
+import NotesSampler from './components/Sampler/NotesSampler';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<h1>Hello world!</h1>} />
+        <Route path="/sampler" element={<NotesSampler />} />
       </Routes>
     </Router>
   )
