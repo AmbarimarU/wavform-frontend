d33e1bd8ad1e313a3e0a45614a17e37ee0db0462
ambarimar ulloa <ambarimar@ambarimars-Air.fios-router.home>
2023-11-08 09:06:43 -0500

    reverb component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 // DEPENDENCIES
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Loader from "./components/Loader/Loader";
 // COMPONENTS
-import Example from './components/Example/Example';
 
+  import Synth from './components/Synth/Synth'
+//const Example = React.lazy(() => import("./components/Example/Example"));
+const Delay = React.lazy(() => import("./components/Delay/Delay"));
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Hello world!</h1>} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <React.Suspense fallback={<Loader />}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<h1>Hello world!</h1>} />
+                    <Route path="/delay" element={<Delay />} />
+                    <Route path='/synth' element={<Synth/>}/>
+                </Routes>
+            </Router>
+        </React.Suspense>
+    );
+
 }
 
-export default App
+export default App;
