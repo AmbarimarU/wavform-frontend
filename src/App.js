447bda98afb37ef51bac7edeb1148863da1c8fb5
Devin Lewis <devinlewis@pursuit.org>
2023-11-09 12:11:36 -0500

    Add all components to App.js

--- src/App.js
+++ src/App.js
@@ -5,34 +5,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader/Loader";
 // COMPONENTS
 
-  import Synth from './components/Synth/Synth'
 //const Example = React.lazy(() => import("./components/Example/Example"));
 
-const Sequencer = React.lazy(() => import("./components/Step Sequencer/StepSequencer"))
-
-
+const Synth = React.lazy(() => import("./components/Synth/Synth"));
+const Reverb = React.lazy(() => import("./components/Reverb/Reverb"));
+const Sequencer = React.lazy(() =>
+    import("./components/Step Sequencer/StepSequencer")
+);
 const Delay = React.lazy(() => import("./components/Delay/Delay"));
-
-  import Synth from './components/Synth/Synth'
-//const Example = React.lazy(() => import("./components/Example/Example"));
-const Delay = React.lazy(() => import("./components/Delay/Delay"));
+const Sampler = React.lazy(() => import("./components/Sampler/NotesSampler"));
 function App() {
     return (
         <React.Suspense fallback={<Loader />}>
             <Router>
                 <Routes>
                     <Route path="/" element={<h1>Hello world!</h1>} />
-
                     <Route path="/delay" element={<Delay />} />
-                    <Route path='/synth' element={<Synth/>}/>
-
-
+                    <Route path="/synth" element={<Synth />} />
+                    <Route path="/sampler" element={<Sampler />} />
+                    <Route path="/reverb" element={<Reverb />} />
                     <Route path="/sequencer" element={<Sequencer />} />
                 </Routes>
             </Router>
         </React.Suspense>
     );
-
 }
 
 export default App;
