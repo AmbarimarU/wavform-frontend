820114e188a07a995513f73b2e052b02d84a40f3
Devin Lewis <devinlewis@pursuit.org>
2023-11-10 13:52:22 -0500

    Changes to tool

--- src/components/MusicTool/MusicTool.js
+++ src/components/MusicTool/MusicTool.js
@@ -9,92 +9,120 @@ const Delay = React.lazy(() => import("../Delay/Delay"));
 const Sampler = React.lazy(() => import("../Sampler/NotesSampler"));
 
 function MusicTool() {
-  const [sequencer, setSequencer] = useState({
-    beat: 0,
-    notes: ["F4", "D#4", "D4", "C4", "A#3", "G#3", "G3", "F3"],
-    playing: false,
-    started: false,
-    tempo: 120,
-  });
+    const [sequencer, setSequencer] = useState({
+        beat: 0,
+        notes: ["F4", "D#4", "D4", "C4", "A#3", "G#3", "G3", "F3"],
+        playing: false,
+        started: false,
+        tempo: 120,
+    });
 
-  const [synthArray, setSynthArray] = useState([]);
+    const [synthArray, setSynthArray] = useState([]);
 
-  const [oscillatorType, setOscillatorType] = useState("sine");
+    const [oscillatorType, setOscillatorType] = useState("sine");
+    let synths = [];
 
-  const createSynths = (count) => {
-    const synths = [];
+    //const player = new Tone.Player();
+    const createSynths = (count) => {
+        synths = [];
+        //console.log("Making with type:" + oscillatorType);
+        for (let i = 0; i < count; i++) {
+            const newSynth = new Tone.Synth({
+                oscillator: {
+                    type: oscillatorType,
+                },
+                envelope: {
+                    attack: 0.8,
+                    decay: 0.5,
+                    sustain: 0.6,
+                    release: 1,
+                },
+            }).toDestination();
+            synths.push(newSynth);
+        }
 
-    for (let i = 0; i < count; i++) {
-      const newSynth = new Tone.Synth({
-        oscillator: {
-          type: oscillatorType,
-        },
-        envelope: {
-          attack: 0.8,
-          decay: 0.5,
-          sustain: 0.6,
-          release: 1,
-        },
-      }).toDestination();
+        setSynthArray(synths);
+        //console.log(synths[1].oscillator.type);
+    };
+    // useEffect(() => {
+    //     createSynths(8);
+    // }, [oscillatorType]);
+    useEffect(() => {
+        createSynths(8);
+    }, []);
 
-      synths.push(newSynth);
-    }
+    useEffect(() => {
+        setSynthArray([]);
+        createSynths(8);
+    }, [oscillatorType]);
 
-    setSynthArray(synths);
-  };
+    return (
+        <div className="musictool">
+            <div className="musictool_header">
+                <select
+                    onChange={(e) => {
+                        //console.log(e.target.value);
+                        //console.log(synthArray);
 
-  useEffect(() => {
-    createSynths(8);
-  }, []);
+                        console.log(e.target.value);
+                        //synths = [];
+                        //console.log("Making with type:" + oscillatorType);
+                        // for (let i = 0; i < 8; i++) {
+                        //     const newSynth = new Tone.Synth({
+                        //         oscillator: {
+                        //             type: oscillatorType,
+                        //         },
+                        //         envelope: {
+                        //             attack: 0.8,
+                        //             decay: 0.5,
+                        //             sustain: 0.6,
+                        //             release: 1,
+                        //         },
+                        //     }).toDestination();
+                        //     synths.push(newSynth);
+                        // }
 
-  useEffect(() => {
-    setSynthArray([])
-
-    createSynths(8);
-  }, [oscillatorType])
-  
-
-  return (
-    <div className="musictool">
-      <div className="musictool_header">
-        <select onChange={(e) => {
-            console.log(e.target.value)
-            console.log(oscillatorType)
-            console.log(synthArray)
-
-            setOscillatorType(e.target.value)
-            }}>
-          <option>sine</option>
-          <option>triangle</option>
-          <option>square</option>
-          <option>sawtooth</option>
-        </select>
-        <select>
-          <option>Sampler</option>
-        </select>
-        <select>
-          <option>Drums/Sounds</option>
-        </select>
-      </div>
-      <div className="musictool_side">Notes / Sounds</div>
-      <div className="musictool_sequencer">
-        <Sequencer
-          synthArray={synthArray}
-          sequencer={sequencer}
-          setSequencer={setSequencer}
-        />
-      </div>
-      <div className="musictool_effects">
-        <h3>Effects</h3>
-      </div>
-      <div className="musictool_reverb">
-        <Reverb />
-      </div>
-      <div className="musictool_delay">
-        <Delay />
-      </div>
-    </div>
-  );
+                        synths = synthArray;
+                        for (let i = 0; i < synths.length; i++) {
+                            synths[i].set({
+                                oscillator: { type: e.target.value },
+                            });
+                        }
+                        setSynthArray(synths);
+                        setOscillatorType(e.target.value);
+                    }}
+                >
+                    <option>sine</option>
+                    <option>triangle</option>
+                    <option>square</option>
+                    <option>sawtooth</option>
+                </select>
+                <select>
+                    <option>Sampler</option>
+                </select>
+                <select>
+                    <option>Drums/Sounds</option>
+                </select>
+            </div>
+            <div className="musictool_side">Notes / Sounds</div>
+            <div className="musictool_sequencer">
+                <Sequencer
+                    synthArray={synthArray}
+                    sequencer={sequencer}
+                    setSequencer={setSequencer}
+                />
+            </div>
+            <div className="musictool_effects">
+                <h3>Effects</h3>
+            </div>
+            <div className="musictool_reverb">
+                <Reverb />
+            </div>
+            <div className="musictool_delay">
+                <Delay />
+            </div>
+        </div>
+    );
 }
 
 export default MusicTool;
