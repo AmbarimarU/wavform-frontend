a52fccb3d1f9eae047ce238c198bc611c40bfb5b
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-11-12 13:17:49 -0500

    Home Component created react logos removed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // DEPENDENCIES
 import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Loader from "./components/Loader/Loader";
 // COMPONENTS
-
+  import Home from './components/Home/Home' 
   import Synth from './components/Synth/Synth'
 //const Example = React.lazy(() => import("./components/Example/Example"));
 const Delay = React.lazy(() => import("./components/Delay/Delay"));
@@ -13,7 +14,7 @@ function App() {
         <React.Suspense fallback={<Loader />}>
             <Router>
                 <Routes>
-                    <Route path="/" element={<h1>Hello world!</h1>} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/delay" element={<Delay />} />
                     <Route path='/synth' element={<Synth/>}/>
                 </Routes>
