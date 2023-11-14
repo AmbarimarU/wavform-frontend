18597a43a784b46b1345427cb4d0f428b6960a58
Devin Lewis <devinlewis@pursuit.org>
2023-11-14 13:21:22 -0500

    Fixes for main

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //const Example = React.lazy(() => import("./components/Example/Example"));
 import Loader from "./components/Loader/Loader";
- 
 
 const About = React.lazy(() => import("./components/About/About"));
-const Login = React.lazy(() => import("./components/Login/AboLoginut"));
+const Login = React.lazy(() => import("./components/Login/Login"));
 const Signup = React.lazy(() => import("./components/Signup/Signup"));
 const Welcome = React.lazy(() => import("./components/Welcome/Welcome"));
 const Lessons = React.lazy(() => import("./components/Lessons/Lessons"));
 const Nav = React.lazy(() => import("./components/Nav/Nav"));
-const ToggleNavbar = React.lazy(() => import("./components/ToggleNavbar/ToggleNavbar"));
+const ToggleNavBar = React.lazy(() =>
+    import("./components/ToggleNavBar/ToggleNavBar")
+);
 const Home = React.lazy(() => import("./components/Home/Home"));
 const Synth = React.lazy(() => import("./components/Synth/Synth"));
 const MusicTool = React.lazy(() => import("./components/MusicTool/MusicTool"));
 const Reverb = React.lazy(() => import("./components/Reverb/Reverb"));
-const Sequencer = React.lazy(() => import("./components/Step Sequencer/StepSequencer"));
+const Sequencer = React.lazy(() =>
+    import("./components/Step Sequencer/StepSequencer")
+);
 const Delay = React.lazy(() => import("./components/Delay/Delay"));
 const Sampler = React.lazy(() => import("./components/Sampler/NotesSampler"));
 const Piano = React.lazy(() => import("./components/Piano/Piano"));
@@ -27,10 +30,10 @@ function App() {
     return (
         <React.Suspense fallback={<Loader />}>
             <Router>
-                <ToggleNavbar>
-                <Nav />
-                </ToggleNavbar>
-                
+                <ToggleNavBar>
+                    <Nav />
+                </ToggleNavBar>
+
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/welcome" element={<Welcome />} />
@@ -38,7 +41,7 @@ function App() {
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/delay" element={<Delay />} />
-                    <Route path='/lessons' element={<Lessons/>}/> 
+                    <Route path="/lessons" element={<Lessons />} />
                     <Route path="/synth" element={<Synth />} />
                     <Route path="/sampler" element={<Sampler />} />
                     <Route path="/reverb" element={<Reverb />} />
