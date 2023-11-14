4c8d55ea4c5094e58ca3731cd9a90bcbe9706583
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-11-14 11:22:05 -0500

    homepages/nav component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Loader from "./components/Loader/Loader";
-// COMPONENTS
-  import Home from './components/Home/Home' 
+// COMPONENT
+  import About from './components/About/About'
+  import Login from './components/Login/Login'
+  import Signup from './components/Signup/Signup'
+  import Welcome from './components/Welcome/Welcome'
+  import Lessons from './components/Lessons/Lessons'
+  import Nav from './components/Nav/Nav'
+  import ToggleNavbar from './components/ToggleNavBar/ToggleNavBar'
+  import Home from './components/Home/Home'
   import Synth from './components/Synth/Synth'
 //const Example = React.lazy(() => import("./components/Example/Example"));
 const Delay = React.lazy(() => import("./components/Delay/Delay"));
@@ -13,10 +20,20 @@ function App() {
     return (
         <React.Suspense fallback={<Loader />}>
             <Router>
+                <ToggleNavbar>
+                <Nav />
+                </ToggleNavbar>
+                
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/welcome" element={<Welcome />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
                     <Route path="/delay" element={<Delay />} />
                     <Route path='/synth' element={<Synth/>}/>
+                    <Route path='/lessons' element={<Lessons/>}/>
+                    
                 </Routes>
             </Router>
         </React.Suspense>
