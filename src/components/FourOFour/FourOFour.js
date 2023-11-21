88253afcba2c123df0641314cb5eb34e45b6b05c
Angel <angel@Angels-MacBook-Air.local>
2023-11-21 11:19:33 -0500

    created fourOfour.js and fourOfour.css

---
+++ src/components/FourOFour/FourOFour.js
@@ -0,0 +1,16 @@
+import "./FourOFour.css"
+
+function FourOFour() {
+  return (
+    <div className="fourOfour">
+      <img
+        src="https://media.giphy.com/media/CBqHOiU5b5JJgJKaeg/giphy.gif"
+        alt="error"
+      />
+
+      <h2>The page you are looking for could not be found!</h2>
+    </div>
+  );
+}
+
+export default FourOFour;
