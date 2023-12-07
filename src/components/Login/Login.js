62ad3ce8828d7550ec67f8a0c54a56752f203053
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-12-07 14:04:22 -0500

    minor fix on login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,64 +1,66 @@
-import React, { useState, useEffect } from "react";
-import { loginUsers } from "../Api/Api";
-import { jwtDecode } from "jwt-decode";
-import useAuthHooks from "../Hooks/Auth/useAuthHooks";
-import { useNavigate } from "react-router-dom";
+import React, {useState, useEffect}from 'react';
+import {loginUsers} from '../Api/Api'
+import { jwtDecode }  from 'jwt-decode';
+import useAuthHooks from '../Hooks/Auth/useAuthHooks';
+import {useNavigate} from 'react-router-dom';
 
-import "./Login.css";
+import './Login.css';
+
 
 function Login({ setUser }) {
-    const [email, setEmail] = useState("");
-    const [password, setpassword] = useState("");
-    const [username, setUsername] = useState("");
-    const [, , checkToken] = useAuthHooks();
+  const [email, setEmail] = useState("")
+  const [password, setpassword] = useState("")
+  const [username, setUsername] = useState("")
+  const [, , checkToken] = useAuthHooks()
 
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        if (checkToken()) {
-            navigate("/get-all-users");
-        }
-    }, []);
+  useEffect(() => {
+    
+    if (checkToken()){
+      navigate("/get-all-users")
+    }
+  }, [])
+   
+  async function handleOnSubmit(e){
+    e.preventDefault()
+    try {
+       let result = await loginUsers({
+        email,
+        password,
+        username,
 
-    async function handleOnSubmit(e) {
-        e.preventDefault();
-        try {
-            let result = await loginUsers({
-                email,
-                password,
-                username,
-            });
-            const jwtToken = result.data.token;
-            console.log(jwtToken);
+       });
+       const jwtToken = result.data.token
+       console.log(jwtToken)
 
-            window.localStorage.setItem("jwtToken", jwtToken);
-            const decodedToken = jwtDecode(jwtToken);
-            console.log(decodedToken);
-
-            setUser({
-                email: decodedToken.email,
-                username: decodedToken.username,
-                id: decodedToken.id,
-            });
-            navigate("/welcome");
-        } catch (error) {
-            console.log(error);
-        }
+       window.localStorage.setItem("jwtToken", jwtToken)
+       const decodedToken = jwtDecode(jwtToken)
+       console.log(decodedToken)
+       
+       setUser({
+        email: decodedToken.email,
+        username: decodedToken.username,
+        id: decodedToken.id
+       });
+       navigate("/welcome")
+    } catch (error) {
+        console.log(error)
     }
   }
   
   
   return (
-    <div className='login'>
-    <div className='login__container'>
-        
-           <h2 className='login__title'>Log in</h2>
-        
-        <form onSubmit={handleOnSubmit} className='login__form'>
+    <div className='form-container' style={{backgroundColor: "#007ead" }} >
+    <div className='form-div'>
+        <div className='form-h1'>
+            <h2>Log in</h2>
+        </div>
+        <form onSubmit={handleOnSubmit} >
             
             
-            <div className='form-container'>
-                <div className='form-field'>
+            <div className='form-input-container'>
+                <div>
                     <input className='form-input'
                     type="text"
                     name="email"
@@ -71,8 +73,8 @@ function Login({ setUser }) {
                 </div>
             
             </div>
-            <div className='form-container'>
-                <div className='form-field'>
+            <div className='form-input-container'>
+                <div>
                     <input className='form-input'
                     type={process.env.NODE_ENV === "production" ? "password" : "text"}
                     name="password"
@@ -86,8 +88,8 @@ function Login({ setUser }) {
                 
             </div>
             
-            <div className='form-container'>
-                <div className='form-field'>
+            <div className='form-input-container'>
+                <div>
                     <input className='form-input'
                     type="text"
                     name="username"
@@ -99,16 +101,16 @@ function Login({ setUser }) {
                     />
                 </div>
                
-                <button className='login-button'>Submit</button>
-
             </div>
            
-            
+             
+                <div className='button-container'>
+                 <button className='login'>Submit</button>
+            </div>
         </form>
     </div>
 </div>
   )
-
 }
 
-export default Login;
+export default Login
