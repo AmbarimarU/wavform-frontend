9df5021cd71d55e8a2f971d6a48b4542273a3d44
Angel <angel@Angels-MacBook-Air.local>
2023-12-11 10:28:51 -0500

    added images for the rhythm lessons

--- src/App.js
+++ src/App.js
@@ -16,30 +16,32 @@ const Signup = React.lazy(() => import("./components/Signup/Signup"));
 const Welcome = React.lazy(() => import("./components/Welcome/Welcome"));
 const Lessons = React.lazy(() => import("./components/Lessons/Lessons"));
 const Nav = React.lazy(() => import("./components/Nav/Nav"));
-const ToggleNavBar = React.lazy(() => import("./components/ToggleNav/ToggleNavBar"));
+const ToggleNavBar = React.lazy(() =>
+  import("./components/ToggleNav/ToggleNavBar")
+);
 const Home = React.lazy(() => import("./components/Home/Home"));
 const Synth = React.lazy(() => import("./components/Synth/Synth"));
 const MusicTool = React.lazy(() => import("./components/MusicTool/MusicTool"));
 const Reverb = React.lazy(() => import("./components/Reverb/Reverb"));
-const Sequencer = React.lazy(() => import("./components/Step Sequencer/StepSequencer"));
+const Sequencer = React.lazy(() =>
+  import("./components/Step Sequencer/StepSequencer")
+);
 const Delay = React.lazy(() => import("./components/Delay/Delay"));
 const Sampler = React.lazy(() => import("./components/Sampler/NotesSampler"));
 const Piano = React.lazy(() => import("./components/Piano/Piano"));
 const GetAllUsers = React.lazy(() =>
-    import("./components/GetAllUsers/GetAllUsers")
+  import("./components/GetAllUsers/GetAllUsers")
 );
 const Profile = React.lazy(() => import("./components/Profile/Profile"));
 const PrivateRoute = React.lazy(() =>
-    import("./components/PrivateRoute/PrivateRoute")
+  import("./components/PrivateRoute/PrivateRoute")
 );
 const Topics = React.lazy(() => import("./components/Topics/Topics"));
 const TopicsDetails = React.lazy(() =>
-    import("./components/Topics/TopicsDetails")
+  import("./components/Topics/TopicsDetails")
 );
 const FourOFour = React.lazy(() => import("./components/FourOFour/FourOFour"));
 // const Sidebar = React.lazy(() => import("./components/Sidebar/Sidebar"));
-const MusicBar = React.lazy(() => import("./components/MusicBar/MusicBar"));
-// const MusicNotes = React.lazy(() => import("./components/MusicBar/MusicNotes"));
 
 function App() {
   const [user, setUser] = useAuthHooks();
@@ -93,8 +95,6 @@ function App() {
           <Route path="/topics/:lessonId" element={<Topics />} />
           <Route path="/topics/topic/:id" element={<TopicsDetails />} />
           <Route path="/*" element={<FourOFour />} />
-          <Route path="/musicbar" element={<MusicBar />} />
-          // <Route path="/musicnotes" element={<MusicNotes />} />
         </Routes>
       </Router>{" "}
     </React.Suspense>
--- src/components/Topics/TopicsDetails.js
+++ src/components/Topics/TopicsDetails.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchTopicDetail } from "../Api/Api";
 import Piano from "../Piano/Piano";
+import BarsAndNotesLesson from "../LessonsImages/BarsAndNotesLesson";
+import TempoLessonImage from "../LessonsImages/TempoLessonImage";
 
 function TopicsDetails() {
   const [singleTopic, setSingleTopic] = useState(null);
@@ -34,16 +36,26 @@ function TopicsDetails() {
     "Piano Practice",
   ];
 
+  const barAndNoteTopic = ["Bars and Types of Notes"];
+  const tempoTopic = ["Tempo"];
+
   const showPiano = pianoTopics.includes(singleTopic?.name);
+  const showBarAndNotesImg = barAndNoteTopic.includes(singleTopic?.name);
+  const showTempo = tempoTopic.includes(singleTopic?.name);
 
   const bulletPoint = singleTopic?.description.split("\n") || [];
 
   const handleNext = () => {
     setCurrentIndex((prev) => Math.min(prev + 1, bulletPoint.length - 1));
+    if(currentIndex < bulletPoint.length - 1){
+      setCurrentIndex(currentIndex + 1)
+    }
   };
 
   const handlePreview = () => {
-    setCurrentIndex((prev) => Math.max(prev - 1, 0));
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
   };
 
   function renderBulletPoints() {
@@ -70,7 +82,7 @@ function TopicsDetails() {
 
     for (let i = chars.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [chars[i], chars[j]] = [chars[j], chars[i]]; 
+      [chars[i], chars[j]] = [chars[j], chars[i]];
     }
 
     const shuffledString = chars.join("");
@@ -88,11 +100,15 @@ function TopicsDetails() {
 
           <div>{showPiano && <Piano />}</div>
 
+          <div>{showBarAndNotesImg && <BarsAndNotesLesson />}</div>
+          <div>{showTempo && <TempoLessonImage />}</div>
           <div>
             {/* <p dangerouslySetInnerHTML={{ __html: formattedDesc }}></p> */}
 
             {renderBulletPoints()}
-            {singleTopic.name === "Piano Practice" ? randomNoteSequence() : null}
+            {singleTopic.name === "Piano Practice"
+              ? randomNoteSequence()
+              : null}
           </div>
           <button onClick={handlePreview} disabled={currentIndex === 0}>
             &laquo; Prev
